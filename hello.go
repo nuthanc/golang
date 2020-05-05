d9ee38df4ba92dfd23cae35cdfa3f241b03e3b66
nuthanc <nuthanc@juniper.net>
2020-05-05 17:47:12 +0530

    Relative import with git mod

--- hello.go
+++
@@ -1,6 +0,0 @@
-package nuthan
-
-// Hello returns Hello Nuthan
-func Hello() string{
-	return "Hello Nuthan"
-}
---
+++ hello/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/nuthanc/golang"
+)
+
+func main(){
+	fmt.Println(nuthan.Speak())
+}
