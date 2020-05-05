c1745a5a45319553f360bcd4f2bfeb7b85ec6bcc
nuthanc <nuthanc@juniper.net>
2020-05-05 16:47:06 +0530

    Update packages

--- another.go
+++ another.go
@@ -1,5 +1,6 @@
 package another
 
+// Speak returns I am speaking
 func Speak() string{
 	return "I am speaking"
 }
---
+++ hello.go
@@ -0,0 +1,6 @@
+package another
+
+// Hello returns Hello Nuthan
+func Hello() string{
+	return "Hello Nuthan"
+}
